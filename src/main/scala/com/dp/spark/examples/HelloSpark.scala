cf777f3a2c217a0b28f8852cd821b8a83205c540
mr-dP <mr-dP@gmail.com>
2020-08-24 00:09:11 +0530

    Basic Spark Project

---
+++ src/main/scala/com/dp/spark/examples/HelloSpark.scala
@@ -0,0 +1,13 @@
+package com.dp.spark.examples
+
+object HelloSpark extends Serializable {
+  //  The Spark framework must Serialize our classes over the network
+
+  def main(args: Array[String]): Unit = {
+
+    
+
+  }
+
+
+}
